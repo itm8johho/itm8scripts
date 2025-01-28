01878b62348d38221f0c0f8b5ea0db4c038e787b
itm8johho <johho@itm8.com>
2025-01-28 14:34:07 +0100

    Update PSToolBox.ps1
    Updated Function Get-HotFixInstallDatesDomain

--- PSToolBox.ps1
+++ PSToolBox.ps1
@@ -462,16 +462,16 @@ Function Get-HotFixInstallDatesDomain { ### Get-HotFixInstallDates for multiple 
           $fInstalledHotfixes | Add-Member -MemberType NoteProperty -Name "IPv4Address" -Value "$($fQueryComputer.IPv4Address)";
           $fInstalledHotfixes; 
         } catch {
-          Write-Host "      An error occurred within the Get-Hotfix command:"
+          Write-Host "`n      An error occurred within the Get-Hotfix command:"
           Write-Host "      $($_.ScriptStackTrace)"
-          Write-Host "    Trying with an Invoked Get-Hotfix command: "
+          Write-Host "`n      Querying Server: $($fQueryComputer.Name) with Invoked Get-Hotfix command: "
           try {
             $fInstalledHotfixes = Invoke-Command -scriptblock { Get-Hotfix | sort InstalledOn -Descending -Unique -ErrorAction SilentlyContinue | Select -First $USING:fHotfixInstallDates  | Select PSComputerName, Description, HotFixID, InstalledBy, InstalledOn;  } -computername $fQueryComputer.Name
             $fInstalledHotfixes | Add-Member -MemberType NoteProperty -Name "OperatingSystem" -Value "$($fQueryComputer.OperatingSystem)";
             $fInstalledHotfixes | Add-Member -MemberType NoteProperty -Name "IPv4Address" -Value "$($fQueryComputer.IPv4Address)";
             $fInstalledHotfixes; 
           } catch {
-            Write-Host "      An error occurred within the Invoked Get-Hotfix command:"
+            Write-Host "`n      An error occurred within the Invoked Get-Hotfix command:"
             Write-Host "      $($_.ScriptStackTrace)`n"
             $fInstalledHotfixes = [pscustomobject]@{
               "PSComputerName" = "$($fQueryComputer.Name)"

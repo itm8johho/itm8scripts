9a7eb36012e698eb13be2579b9ade8b289251839
itm8johho <johho@itm8.com>
2025-06-16 08:45:04 +0200

    Update PSToolBox.ps1
    Updated Function Get-ADUserPasswordNeverExpires
    Output Order

--- PSToolBox.ps1
+++ PSToolBox.ps1
@@ -597,7 +597,7 @@ Function Get-ADUserPasswordNeverExpires {## Get Password Never Expires for AD Us
   ## Script
     Show-Title $fTitle;
     $fDaysInactiveTimestamp = [DateTime]::Now.AddDays(-$($fDaysInactive));
-    $fResult = Get-ADUser -Filter * -Properties Name, LockedOut, PasswordNeverExpires, pwdlastSet | where { $_.passwordNeverExpires -eq $true } | Sort Name | Select-Object Name, SamAccountName, LockedOut, @{n="PwdNeverExpires";e={$_.PasswordNeverExpires}}, @{n="PwdLastSet";e={[datetime]::FromFileTime($_."PwdLastSet").ToString("yyyy-MM-dd HH:mm:ss")}}, Enabled;
+    $fResult = Get-ADUser -Filter * -Properties Name, LockedOut, PasswordNeverExpires, pwdlastSet | where { $_.passwordNeverExpires -eq $true } | Sort Name | Select-Object Name, SamAccountName, Enabled, LockedOut, @{n="PwdNeverExpires";e={$_.PasswordNeverExpires}}, @{n="PwdLastSet";e={[datetime]::FromFileTime($_."PwdLastSet").ToString("yyyy-MM-dd HH:mm:ss")}};
   ## Output
     #$fResult;
   ## Exports
